d74ab7e7377d3365d612b3c899514d648c04b13c
nikilandgello <nikoscha22012004@gmail.com>
2024-08-29 17:55:33 +0300

    branches+cycles

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function checkForSpam(message) {
+    let lowerMessage = message.toLowerCase()
+
+    if (lowerMessage.includes('spam') || lowerMessage.includes('sale')){
+        return true;
+    } else {
+        return false;
+    };
+};
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
